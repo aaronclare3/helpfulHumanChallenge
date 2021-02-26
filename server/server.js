46e90cf171e26c1b90fc4f898c1e2d4473d61495
aaronclare3 <aaronclare88@gmail.com>
2021-02-25 17:11:38 -0800

    changed port to env variable

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 6969 || 3000;
+const PORT = process.env.PORT || 5000;
 const colorData = require("./colorsData.json");
 const graphql = require("graphql");
 const cors = require("cors");
