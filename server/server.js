446d7a3f78475c12b8c2ebbf7e820011e799fea3
aaronclare3 <aaronclare88@gmail.com>
2021-02-25 16:55:07 -0800

    pushing another change for heroku

--- server/server.js
+++ server/server.js
@@ -59,6 +59,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
-app.listen({ port: PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+app.listen(PORT || 5000, () => {
+  console.log("server running!");
 });
